ccbee51ad76fddf81d23df1497d0a8313fe7bde1
ttt <test@ex.com>
2022-09-25 16:19:53 +0900

    create grover algo with 3 Qubits

--- algorithms/grover_v1.py
+++ algorithms/grover_v1.py
@@ -19,14 +19,8 @@ def main(args0,args1):
 	print(input_zeros(NQ_Data,NQ_Index))
 	
 	QC = QuCircuit(NQ_Data,NQ_Index)
-	if args0 == 0:
-		FLAG_GATE  = "0"
-	elif args0 == 1:
-		FLAG_GATE  = "1"
-	elif args0 == 2:
-		FLAG_GATE  = "2"
-	elif args0 == 3:
-		FLAG_GATE  = "3"
+	
+	FLAG_GATE = args0
 	
 	if args1 == 1:
 		FLAG_SIM = "ON"
@@ -37,26 +31,90 @@ def main(args0,args1):
 		print("--Answer : 000--")
 	elif FLAG_GATE == "1":
 		print("--Answer : 001--")
+	elif FLAG_GATE == "2":
+		print("--Answer : 010--")
+	elif FLAG_GATE == "3":
+		print("--Answer : 011--")
+	elif FLAG_GATE == "4":
+		print("--Answer : 100--")
+	elif FLAG_GATE == "5":
+		print("--Answer : 101--")
+	elif FLAG_GATE == "6":
+		print("--Answer : 110--")
+	elif FLAG_GATE == "7":
+		print("--Answer : 111--")
+	else:
+		print("--Answer : 000--")
+
 	for i in range(NUM_ITR):
 		# Oracle
-		if FLAG_GATE == "0":
-			print("Oracle x",i+1,":Diffuser x",i)
+		print("Oracle x",i+1,":Diffuser x",i)
+		if FLAG_GATE == 0:
 			QC.x(range(0,NQ_ALL))
 			QC.add()
 			QC.cz(range(0,NQ_ALL-1),NQ_ALL-1)
 			QC.add()
 			QC.x(range(0,NQ_ALL))
 			QC.add()
-			QC.show_ALL()
-		elif FLAG_GATE == "1":
-			print("Oracle x",i+1,":Diffuser x",i)
+		elif FLAG_GATE == 1:
 			QC.x(range(0,2))
 			QC.add()
 			QC.cz(range(0,NQ_ALL-1),NQ_ALL-1)
 			QC.add()
 			QC.x(range(0,2))
 			QC.add()
-			QC.show_ALL()
+		elif FLAG_GATE == 2:
+			QC.x(0)
+			QC.x(2)
+			QC.add()
+			QC.cz(range(0,NQ_ALL-1),NQ_ALL-1)
+			QC.add()
+			QC.x(0)
+			QC.x(2)
+			QC.add()
+		elif FLAG_GATE == 3:
+			QC.x(0)
+			QC.add()
+			QC.cz(range(0,NQ_ALL-1),NQ_ALL-1)
+			QC.add()
+			QC.x(0)
+			QC.add()
+		elif FLAG_GATE == 4:
+			QC.x(range(1,3))
+			QC.add()
+			QC.cz(range(0,NQ_ALL-1),NQ_ALL-1)
+			QC.add()
+			QC.x(range(1,3))
+			QC.add()
+		elif FLAG_GATE == 5:
+			QC.x(1)
+			QC.add()
+			QC.cz(range(0,NQ_ALL-1),NQ_ALL-1)
+			QC.add()
+			QC.x(1)
+			QC.add()
+		elif FLAG_GATE == 6:
+			QC.x(2)
+			QC.add()
+			QC.cz(range(0,NQ_ALL-1),NQ_ALL-1)
+			QC.add()
+			QC.x(2)
+			QC.add()
+		elif FLAG_GATE == 7:
+			#QC.x(range(0,2))
+			#QC.add()
+			QC.cz(range(0,NQ_ALL-1),NQ_ALL-1)
+			#QC.add()
+			#QC.x(range(0,2))
+			QC.add()
+		else: # FLAG_GATE == 0
+			QC.x(range(0,NQ_ALL))
+			QC.add()
+			QC.cz(range(0,NQ_ALL-1),NQ_ALL-1)
+			QC.add()
+			QC.x(range(0,NQ_ALL))
+			QC.add()
+		QC.show_ALL()
 		# Diffuser
 		print("Oracle x",i+1,":Diffuser x",i+1)
 		QC.h(range(0,NQ_ALL))
